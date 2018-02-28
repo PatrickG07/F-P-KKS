b0d1f61b33294bbd166980cbc19fd60291b96b89
PatrickG07 <gartenmannpatrick@gmx.ch>
2018-02-28 14:18:10 +0100

    canging Create Class

--- FPKKS/app/src/main/java/ch/fp/fp_kks/CreateActivity.java
+++ FPKKS/app/src/main/java/ch/fp/fp_kks/CreateActivity.java
@@ -5,17 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateActivity extends AppCompatActivity {
+
+    EditText tvText1;
+    EditText tvText2;
+    DatabaseHelper mDatabaseHelper;
+
+    String newEntry1, newEntry2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
-        Button btnBack = (Button) findViewById(R.id.btnBack);
+        mDatabaseHelper = new DatabaseHelper(this);
 
+        Button btnBack = (Button) findViewById(R.id.btnBack);
         Button btnNew = (Button) findViewById(R.id.btnNew);
+
+        tvText1 = (EditText) findViewById(R.id.tvText1);
+        tvText2 = (EditText) findViewById(R.id.tvText2);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +40,9 @@ public class CreateActivity extends AppCompatActivity {
         btnNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                newEntry1 = String.valueOf(tvText1.getText());
+                newEntry2 = String.valueOf(tvText2.getText());
+                mDatabaseHelper.addData(newEntry1, newEntry2);
             }
         });
     }
