edf770db37d8e8f3f24a004f9e8c6eeacfe0f000
PatrickG07 <gartenmannpatrick@gmx.ch>
2018-02-28 13:36:43 +0100

    Database
    +database
    +Backgroud CLass

---
+++ FPKKS/app/src/main/java/ch/fp/fp_kks/Background.java
@@ -0,0 +1,46 @@
+package ch.fp.fp_kks;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Background for using int ids and String text in every class
+ * <p>
+ * Created by Patrick on 24.08.2017.
+ */
+public class Background extends Service {
+
+    static int ids;
+    static String text;
+
+    /**
+     * @param intent;
+     */
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    /**
+     * @param intent;
+     * @param flags;
+     * @param startId;
+     */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_NOT_STICKY;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "on Destroy call", Toast.LENGTH_SHORT).show();
+    }
+}
