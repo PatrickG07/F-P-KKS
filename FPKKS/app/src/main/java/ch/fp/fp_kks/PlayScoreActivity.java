a2518fc27cfe397083496a609cfc8ee4c38fcdf4
Fabian Kurt <pumba99@outlook.com>
2018-03-05 19:13:47 +0100

    Update

--- FPKKS/app/src/main/java/ch/fp/fp_kks/PlayScoreActivity.java
+++ FPKKS/app/src/main/java/ch/fp/fp_kks/PlayScoreActivity.java
@@ -1,13 +1,18 @@
 package ch.fp.fp_kks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class PlayScoreActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_score);
+       
     }
+
+
 }
