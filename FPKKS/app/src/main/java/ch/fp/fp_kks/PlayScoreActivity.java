5aa2b34f0e24cbb719c624ef87b86bbfe1872776
Fabian Kurt <pumba99@outlook.com>
2018-03-06 09:42:36 +0100

    PlayScore

--- FPKKS/app/src/main/java/ch/fp/fp_kks/PlayScoreActivity.java
+++ FPKKS/app/src/main/java/ch/fp/fp_kks/PlayScoreActivity.java
@@ -3,15 +3,39 @@ package ch.fp.fp_kks;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayScoreActivity extends AppCompatActivity {
 
-
+    float rainfall[] ={24.0f, 76.0f};
+    String score[] ={"Richtig", "Falsch"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_score);
+        setupPieChart();
+    }
 
+    private void setupPieChart() {
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for(int i = 0; 1 <rainfall.length; i++){
+            pieEntries.add(new PieEntry(rainfall[i], score[i]));
+        }
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Richtig & Falsch");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        PieChart chart = (PieChart) findViewById(R.id.PCScore);
+        chart.setData(data);
+        chart.invalidate();
     }
 
 
