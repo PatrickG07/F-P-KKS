d7bfea228116ea0555a5dc43564c3a40272e66f6
Fabian Kurt <pumba99@outlook.com>
2018-03-06 10:43:30 +0100

    Info Update

---
+++ FPKKS/app/src/main/java/ch/fp/fp_kks/InfoActivity.java
@@ -0,0 +1,13 @@
+package ch.fp.fp_kks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
