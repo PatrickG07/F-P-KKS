5e36ae53cabddb75de07075db1e6a1b500291071
Fabian Kurt <pumba99@outlook.com>
2018-03-06 10:52:58 +0100

    update

--- FPKKS/app/src/main/java/ch/fp/fp_kks/MainActivity.java
+++ FPKKS/app/src/main/java/ch/fp/fp_kks/MainActivity.java
@@ -96,8 +96,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.asInfo) {
+            Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
